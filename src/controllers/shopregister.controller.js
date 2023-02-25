029331cdcd786c65714fbd6ae24b56a90c722e9a
UyarchiSolutions <gitadmin@uyarchi.com>
2023-02-25 13:43:13 +0530

    sdfg

--- src/controllers/shopregister.controller.js
+++ src/controllers/shopregister.controller.js
@@ -138,7 +138,6 @@ const imageUpload_For_Issues = catchAsync(async (req, res) => {
   const shop = await registerShop.imageUpload_For_Issues(req.params.id, req.body);
   if (req.files) {
     req.files.forEach(function (files, index, arr) {
-      shop.image == null ? (shop.image = []) : shop.image;
       shop.image.push('images/issue/' + files.filename);
     });
   }

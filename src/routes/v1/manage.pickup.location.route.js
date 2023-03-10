5dadcc416832d06db0eb3c963ade534069ae3880
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-03-10 12:43:50 +0530

    S

--- src/routes/v1/manage.pickup.location.route.js
+++ src/routes/v1/manage.pickup.location.route.js
@@ -7,7 +7,7 @@ router.route('/').post(authorization, pickup.array('photoCapture'), managePickup
 
 router.route('/getAll/:userId/:date/:page').get(managePickupController.getAllManagepickup);
 router.route('/:id').get(managePickupController.getManagePickupById);
-router.route('/update/pickup/:id').put(managePickupController.update_pickup_location);
+router.route('/update/pickup').put(managePickupController.update_pickup_location);
 router.route('/getAll/pickup/:userId/:date/:todate').get(managePickupController.getAllManagepickupLocation);
 
 router.route('/getAll/pickuploaction').get(authorization, managePickupController.getallPickuplocation);

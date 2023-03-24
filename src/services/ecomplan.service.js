92742649620cedf5edea629d3c93e3f42977f5ed
muthamizhyadav <muthamizhyadav@gmail.com>
2023-03-24 10:31:02 +0530

    sdf

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -6041,7 +6041,7 @@ const multipleCancel = async (body) => {
     values = await streamingorderProduct.findByIdAndUpdate({ _id: e }, { status: body.status }, { new: true });
     let totalOrder = await streamingorderProduct.find({ orderId: orderId }).count();
     let pendingCount = await streamingorderProduct.find({ orderId: orderId, status: 'Pending' }).count();
-    let streamorder = await findById(orderId);
+    let streamorder = await streamingOrder.findById(orderId);
 
     if (pendingCount == 0 && streamorder.orderStatus != 'confirmed' && streamorder.orderStatus != 'ready') {
       await streamingOrder.findByIdAndUpdate({ _id: orderId }, { orderStatus: 'ready' }, { new: true });

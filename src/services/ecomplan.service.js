7f0bc0ad18b094b0b3462a23cf0a1864990aa00a
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-03-28 15:35:39 +0530

    sd

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -1739,16 +1739,16 @@ const cancel_stream = async (req) => {
 };
 
 const end_stream = async (req) => {
-  // let value = await Streamrequest.findByIdAndUpdate(
-  //   { _id: req.query.id },
-  //   { status: 'Completed', streamEnd_Time: moment(), end_Status: 'HostLeave' },
-  //   { new: true }
-  // );
+  let value = await Streamrequest.findByIdAndUpdate(
+    { _id: req.query.id },
+    { status: 'Completed', streamEnd_Time: moment(), end_Status: 'HostLeave' },
+    { new: true }
+  );
   req.io.emit(req.query.id + '_stream_end', {value:true})
-  // let assginStream = await StreamrequestPost.find({ streamRequest: req.query.id });
-  // assginStream.forEach(async (a) => {
-  //   await StreamPost.findByIdAndUpdate({ _id: a.postId }, { status: 'Completed' }, { new: true });
-  // });
+  let assginStream = await StreamrequestPost.find({ streamRequest: req.query.id });
+  assginStream.forEach(async (a) => {
+    await StreamPost.findByIdAndUpdate({ _id: a.postId }, { status: 'Completed' }, { new: true });
+  });
   return { value: true };
 };
 

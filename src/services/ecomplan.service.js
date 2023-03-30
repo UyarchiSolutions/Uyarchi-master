5af2ca8cac721e82dd626abecf77f20c4c23fa7c
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-03-30 10:55:40 +0530

    ad

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -3041,7 +3041,8 @@ const unregisetr_strean_instrest = async (req) => {
           streamName: streamDetails.streamName,
           title: streamDetails.streamName + " Stream Is Available to Watch"
         })
-      req.io.emit(next.shopId + "_stream_CFM", notification)
+      let count = await shopNotification.find({ shopId: req.shopId, status: 'created' }).count();
+      req.io.emit(next.shopId + "_stream_CFM", { notification, count: count })
       next.save();
     }
   }

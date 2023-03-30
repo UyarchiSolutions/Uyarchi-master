cef21dd91a618eeaef43f30c1aca23aea4fb773f
muthamizhselvan R <muthamizhyadav@gmail.com>
2023-03-30 14:51:35 +0530

    sdfg

--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -1320,7 +1320,11 @@ const find_and_update_one = async (req) => {
   let plan = await Streamplan.findById(myplan.planId);
   let datess = new Date().setTime(new Date(streamss.startTime).getTime() + plan.Duration * 60 * 1000);
 
-  let value = await Streamrequest.findByIdAndUpdate({ _id: req.query.id }, { ...req.body, ...{ startTime: startTime, endTime: datess, streamEnd_Time: datess, } }, { new: true });
+  let value = await Streamrequest.findByIdAndUpdate(
+    { _id: req.query.id },
+    { ...req.body, ...{ startTime: startTime, endTime: datess, streamEnd_Time: datess } },
+    { new: true }
+  );
   let posts = value.post;
   console.log(posts, req.body.addpost);
   req.body.addpost.forEach(async (a) => {
@@ -1744,7 +1748,7 @@ const end_stream = async (req) => {
     { status: 'Completed', streamEnd_Time: moment(), end_Status: 'HostLeave' },
     { new: true }
   );
-  req.io.emit(req.query.id + '_stream_end', {value:true})
+  req.io.emit(req.query.id + '_stream_end', { value: true });
   let assginStream = await StreamrequestPost.find({ streamRequest: req.query.id });
   assginStream.forEach(async (a) => {
     await StreamPost.findByIdAndUpdate({ _id: a.postId }, { status: 'Completed' }, { new: true });
@@ -1883,7 +1887,7 @@ const get_all_streams = async (req) => {
     },
     {
       $addFields: {
-        allot_chat_name: { $ifNull: ['$allot_chat_lookup.Name', "$allot_chat"] },
+        allot_chat_name: { $ifNull: ['$allot_chat_lookup.Name', '$allot_chat'] },
       },
     },
     {
@@ -1902,7 +1906,7 @@ const get_all_streams = async (req) => {
     },
     {
       $addFields: {
-        allot_host_1_name: { $ifNull: ['$allot_host_1_lookup.Name', "$allot_host_1"] },
+        allot_host_1_name: { $ifNull: ['$allot_host_1_lookup.Name', '$allot_host_1'] },
       },
     },
     {
@@ -1921,7 +1925,7 @@ const get_all_streams = async (req) => {
     },
     {
       $addFields: {
-        allot_host_2_name: { $ifNull: ['$allot_host_2_lookup.Name', "$allot_host_2"] },
+        allot_host_2_name: { $ifNull: ['$allot_host_2_lookup.Name', '$allot_host_2'] },
       },
     },
     {
@@ -1940,7 +1944,7 @@ const get_all_streams = async (req) => {
     },
     {
       $addFields: {
-        allot_host_3_name: { $ifNull: ['$allot_host_3_lookup.Name', "$allot_host_3"] },
+        allot_host_3_name: { $ifNull: ['$allot_host_3_lookup.Name', '$allot_host_3'] },
       },
     },
     {
@@ -1995,7 +1999,6 @@ const get_all_streams = async (req) => {
         allot_host_1_name: 1,
         allot_host_2_name: 1,
         allot_host_3_name: 1,
-
       },
     },
 
@@ -2961,8 +2964,8 @@ const regisetr_strean_instrest = async (req) => {
       participents.noOfParticipants > count
         ? 'Confirmed'
         : participents.noOfParticipants + participents.noOfParticipants / 2 > count
-          ? 'RAC'
-          : 'Waiting';
+        ? 'RAC'
+        : 'Waiting';
     await Dates.create_date(findresult);
   } else {
     if (findresult.status != 'Registered') {
@@ -2971,8 +2974,8 @@ const regisetr_strean_instrest = async (req) => {
         participents.noOfParticipants > count
           ? 'Confirmed'
           : participents.noOfParticipants + participents.noOfParticipants / 2 > count
-            ? 'RAC'
-            : 'Waiting';
+          ? 'RAC'
+          : 'Waiting';
       findresult.eligible = participents.noOfParticipants > count;
       findresult.status = 'Registered';
       await Dates.create_date(findresult);
@@ -6934,8 +6937,84 @@ const getDetails = async (id) => {
       },
     },
     {
-      $addFields: { Amount: { $multiply: ['$purchase_quantity', '$purchase_price'] } },
+      $addFields: { Amount: { $toInt: { $multiply: ['$purchase_quantity', '$purchase_price'] } } },
     },
+
+    {
+      $lookup: {
+        from: 'streamrequestposts',
+        localField: 'postId',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'streamposts',
+              localField: 'postId',
+              foreignField: '_id',
+              pipeline: [{ $match: { bookingAmount: 'yes' } }],
+              as: 'post',
+            },
+          },
+          {
+            $unwind: {
+              preserveNullAndEmptyArrays: true,
+              path: '$post',
+            },
+          },
+        ],
+        as: 'requestPOst',
+      },
+    },
+    {
+      $unwind: {
+        preserveNullAndEmptyArrays: true,
+        path: '$requestPOst',
+      },
+    },
+    {
+      $addFields: { bookingAmount: '$requestPOst.post.bookingAmount' },
+    },
+    {
+      $lookup: {
+        from: 'slabdetails',
+        let: { formAmount: '$Amount', endAmount: '$Amount', bookingAmount: '$bookingAmount' },
+        pipeline: [
+          {
+            $match: {
+              $and: [
+                { $expr: { $gte: ['$$formAmount', '$formAmount'] } },
+                { $expr: { $lte: ['$$endAmount', '$endAmount'] } },
+                { $expr: { $eq: ['$$bookingAmount', 'yes'] } },
+              ],
+            },
+          },
+          { $limit: 1 },
+        ],
+        as: 'slab',
+      },
+    },
+    {
+      $unwind: {
+        preserveNullAndEmptyArrays: true,
+        path: '$slab',
+      },
+    },
+    {
+      $addFields: { slabPercentage: { $ifNull: ['$slab.slabPercentage', 0] } },
+    },
+    {
+      $addFields: {
+        bookingAmount: {
+          $divide: [
+            {
+              $multiply: ['$slabPercentage', '$Amount'],
+            },
+            100,
+          ],
+        },
+      },
+    },
+
     {
       $project: {
         _id: 1,
@@ -6944,6 +7023,9 @@ const getDetails = async (id) => {
         purchase_price: 1,
         Amount: 1,
         product: '$products.productTitle',
+        // bookingAmount: 1,
+        slabPercentage: 1,
+        bookingAmount: 1,
       },
     },
   ]);
